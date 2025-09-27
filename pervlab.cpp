86d54e364bb3c8912706270912f74961ddae7680
PashoktheprogrammisT <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-09-27 21:13:58 +0300

    Add files via upload

---
+++ pervlab.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+int main() {
+    string input; getline(cin, input);
+    if (isPalindrome(input)) {
+        cout << "PALINDROM" << "\n";
+    }
+    else cout << "NE PALINDROM" << "\n";
+}*/
