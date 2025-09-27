1377844ae34b3702bdef04af242bcd52df4ad4fa
PashoktheprogrammisT <160039558+PashoktheprogrammisT@users.noreply.github.com>
2025-09-27 21:18:35 +0300

    Create palindrom.cpp

---
+++ src/palindrom.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "palindrom.h"
+bool isPalindrome(const std::string& str) {
+    int left = 0;
+    int right = str.length()-1;
+
+    while (left < right) {
+        if (str[left] !=str[right]) {
+            return false;
+        }
+        left++;right--;
+    }
+    return true;
+}
